6ab75fa0a492ab650101347902be75d5fbdc7fa8
Brenda Jacobsen <brendajacobsen@Brendas-iMac.local>
2020-01-11 22:57:10 -0700

    commit

--- myScript.js
+++ myScript.js
@@ -9,5 +9,6 @@ try{
         "currentdate2"
     ).textContent = new Date().toLocaleDateString("en-US", options);
 } catch (e) {
-    alert("Error with code or your browser does not support Locale");
+    alert(document.lastModified);
+    // returns: Saturday, January 12, 2020 10:56:42);
 }
