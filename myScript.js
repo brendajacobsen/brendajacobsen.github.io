6a52c79e64750c471cda7c8d84e4533ee22993f6
Brenda Jacobsen <brendajacobsen@Brendas-iMac.local>
2020-01-17 16:07:07 -0700

    commit

--- myScript.js
+++ myScript.js
@@ -3,5 +3,5 @@ let year = date.getFullYear();
 
 let lastModify = document.lastModified;
 
-document.getElementById("currentdate2").textContent = year;
+document.getElementById("currentyear").textContent = year;
 document.getElementById("lastupdate").textContent = "Last Updated: " + lastModify;
