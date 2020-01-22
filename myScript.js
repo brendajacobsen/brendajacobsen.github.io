48c7a55477b7aff4ea749c8faf0b14f3d1468c86
Brenda Jacobsen <brendajacobsen@Brendas-iMac.local>
2020-01-21 23:02:08 -0700

    commit

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
-let date = new Date();
-let year = date.getFullYear();
-
-let lastModify = document.lastModified;
-
-document.getElementById("currentyear").textContent = year;
-document.getElementById("lastmod").textContent = "Last Updated: " + lastModify;
+ //const options={minute: "numeric", hour: "numeric", day: "numeric", month: "long", }
+ const options= {year:"numeric"};
+          
+ document.getElementById('currentyear').textContent=new Date().toLocaleDateString("en-US", options);
+// const modoptions ={ second: "numeric", minute: "numeric", hour: "numeric", day: "numeric", month:"numeric", year: "numeric"};
+ //document.getElementById('lastModified').textContent=new Date(document.lastModified);
+ document.getElementById('lastModified').textContent=document.lastModified;
