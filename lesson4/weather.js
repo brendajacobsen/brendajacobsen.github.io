68e6e2bae1f8b0ec14f8561728b039578ab295a6
Brenda Jacobsen <brendajacobsen@Brendas-iMac.local>
2020-02-01 23:16:51 -0700

    commit

--- lesson4/weather.js
+++ lesson4/weather.js
@@ -4,3 +4,9 @@ hambutton.addEventListener("click", toggleMenu, false);
 function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
 }
+var d = new Date();
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var dayMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var fecha = dayNames[d.getDay()] +", " + d.getDate() + " "+ dayMonths[d.getMonth()] + " "+ d.getFullYear();
+
+document.getElementById("currentdate").innerHTML=fecha;
