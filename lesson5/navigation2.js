833d760ba062e5899ab5bd2b3c701be44920cb53
Brenda Jacobsen <brendajacobsen@Brendas-iMac.local>
2020-02-13 00:47:15 -0700

    commit

--- lesson5/navigation2.js
+++ lesson5/navigation2.js
@@ -4,3 +4,10 @@ hambutton.addEventListener("click", toggleMenu, false);
 function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
 }
+function showBanner() {
+  var today= new Date();
+  var wkday= today.getDay();
+  if (wkday == 5){
+  document.getElementById("banner").classList.toggle("noshow");
+ 
+}
